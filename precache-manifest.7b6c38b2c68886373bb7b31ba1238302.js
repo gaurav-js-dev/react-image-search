923bb0b8a71dff45ae8541605d49589917e716c3
Gaurav Sharma <gaurav.haxor@gmail.com>
2021-05-15 01:21:59 +0530

    Updates

---
+++ precache-manifest.7b6c38b2c68886373bb7b31ba1238302.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad15af22adff65afd30db9b54b28da7",
+    "url": "/what-the-splash/index.html"
+  },
+  {
+    "revision": "98e7ae351709d5bd0a69",
+    "url": "/what-the-splash/static/css/main.d6375d5f.chunk.css"
+  },
+  {
+    "revision": "81eb3fadf5c787fd98c5",
+    "url": "/what-the-splash/static/js/2.45f5d721.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/what-the-splash/static/js/2.45f5d721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e7ae351709d5bd0a69",
+    "url": "/what-the-splash/static/js/main.b805d296.chunk.js"
+  },
+  {
+    "revision": "1aa6b21b4ca18d4d1f12",
+    "url": "/what-the-splash/static/js/runtime-main.45938b4c.js"
+  }
+]);
