12d2b40a9cea893ad8a19d6bd42aab72d0139c98
Gaurav Sharma <gaurav.haxor@gmail.com>
2021-05-30 11:56:25 +0530

    Updates

---
+++ precache-manifest.4157bdbc1c2edcb04fd563dc5b155a3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c70cb08bf04355aab52bb72aa0757d",
+    "url": "/react-image-search/index.html"
+  },
+  {
+    "revision": "c6e7706642a5b33c4ddd",
+    "url": "/react-image-search/static/css/main.80db6191.chunk.css"
+  },
+  {
+    "revision": "0ef451e38a1f2bc83835",
+    "url": "/react-image-search/static/js/2.d62dc805.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-image-search/static/js/2.d62dc805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6e7706642a5b33c4ddd",
+    "url": "/react-image-search/static/js/main.7e36768b.chunk.js"
+  },
+  {
+    "revision": "501f26735f13c0a7d5bc",
+    "url": "/react-image-search/static/js/runtime-main.74e9459e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/what-the-splash/precache-manifest.ed99ade54e078a3584e19ef468667301.js"
+  "/react-image-search/precache-manifest.4157bdbc1c2edcb04fd563dc5b155a3a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/what-the-splash/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-image-search/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
